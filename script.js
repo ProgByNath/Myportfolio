47251419034fc8efc780adf3b2b03264a845c88f
ProgByNath <138643359+ProgByNath@users.noreply.github.com>
2023-08-17 11:20:14 +0800

    nath

---
+++ script.js
@@ -0,0 +1,31 @@
+VANTA.CELLS({
+    el: ".vading, project_container",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    color1: 0x93a4a4,
+    color2: 0x1000,
+    size: 0.70,
+    speed: 3.40
+  })
+
+  VANTA.CELLS({
+    el: "..vading, project_container",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: "100%", // Set the minimum height to 100vh (viewport height)
+    minWidth: "100%",  // Set the minimum width to 100% (viewport width)
+    scale: 1.00,
+    color1: 0x93a4a4,
+    color2: 0x1000,
+    size: 0.70,
+    speed: 3.40
+  });
+
+
+
+
